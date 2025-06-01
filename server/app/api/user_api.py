d2a0ed01f2a6183838f4d3fb2c699d1122779f8c
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2025-06-01 21:17:08 +0300

    add endpoint for user update

--- server/app/api/user_api.py
+++ server/app/api/user_api.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, make_response
+from flask import Blueprint, make_response, request
 from flask_jwt_extended import get_jwt_identity, unset_jwt_cookies, get_jwt
+from pydantic import ValidationError
+from sqlalchemy.exc import SQLAlchemyError
 
 from app.models.user_model import User
+from app.schemas.user_schemas import UserUpdateSchema
 from app.utils.decorators import logged_in_required
+from app.utils.extensions import db
 from app.utils.responses import create_response
 
 users = Blueprint('users', __name__)
@@ -36,3 +40,71 @@ def get_current_user():
         )
         unset_jwt_cookies(response)
         return response
+
+
+@users.route('/update', methods=('PUT',))
+@logged_in_required
+def update_current_user():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    if not user:
+        response = make_response(create_response(
+            status_code=404,
+            message='User not found'
+        ))
+        unset_jwt_cookies(response)
+        return response
+
+    data = request.get_json()
+    if not data:
+        return create_response(
+            status_code=400,
+            message='No data provided for update'
+        )
+
+    try:
+        validated_data = UserUpdateSchema(**data)
+    except ValidationError as e:
+        return create_response(
+            status_code=400,
+            message='Invalid input',
+            details=e.errors()
+        )
+    except Exception as e:
+        return create_response(
+            status_code=500,
+            message='Internal server error',
+            details=str(e)
+        )
+
+    existing_user = User.query.filter(
+        (User.email == validated_data.email) | (User.username == validated_data.username)).first()
+    if existing_user:
+        if existing_user.email == validated_data.email:
+            return create_response(
+                status_code=400,
+                message='User with this email already exists'
+            )
+        if existing_user.username == validated_data.username:
+            return create_response(
+                status_code=400,
+                message='User with this username already exists'
+            )
+
+    try:
+        update_data = validated_data.model_dump(exclude_unset=True)
+        user.update_from_dict(update_data)
+        db.session.commit()
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        return create_response(
+            status_code=500,
+            message='Database error',
+            details=str(e)
+        )
+
+    return create_response(
+        status_code=200,
+        message='User updated successfully',
+        data=user.to_dict()
+    )
