6f873ff4b7b3b3d4bf6194403238d4f20115e11f
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2025-06-03 22:52:21 +0300

    register budgets blueprint

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from app.api.auth_api import auth
+from app.api.budget_api import budgets
 from app.api.users_api import users
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
 api.register_blueprint(auth, url_prefix='/auth')
 api.register_blueprint(users, url_prefix='/users')
+api.register_blueprint(budgets, url_prefix='/budgets')
