6c4d36d3ac44287b230e0f9a5d15d526b2531049
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2025-06-11 22:01:18 +0300

    fix

--- client/config.js
+++
@@ -1,4 +0,0 @@
-/**
- * URL for the API server. Retrieves the value from the environment variable REACT_APP_API_URL or defaults to 'http://localhost:5000'.
- */
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 /**
- * Constants for the application configuration.
+ * URL for the API server. Retrieves the value from the environment variable REACT_APP_API_URL or defaults to 'http://localhost:5000'.
  */
 export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
